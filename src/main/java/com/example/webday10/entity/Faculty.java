bc69d1d578930ac43adae2b808b896d5aca570a1
Huy Tran <huy.tran@evizi.com>
2020-10-16 20:05:34 +0700

    initial commit

---
+++ src/main/java/com/example/webday10/entity/Faculty.java
@@ -0,0 +1,50 @@
+package com.example.webday10.entity;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "FACULTIES")
+public class Faculty {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	@OneToMany(mappedBy = "faculty")
+	private Set<Student> students;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(Set<Student> students) {
+		this.students = students;
+	}
+	
+	
+}
